858e66cf9adb0ae6e29d5c3d7995d2a6a7cd21ce
Annie Zhang <Annie Zhang>
2017-04-29 15:45:15 -0400

    Began new website

---
+++ Cornell Cinema/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="styles/style.css">
+	<title>Welcome to Cornell Cinema</title>
+</head>
+<body>
+	
+</body>
+<footer>
+	
+</footer>
+</html>
