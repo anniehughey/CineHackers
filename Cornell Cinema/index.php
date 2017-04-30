759e4e833289eaf2c149df81a78bbf4cf3504a53
Annie Zhang <Annie Zhang>
2017-04-30 15:46:35 -0400

    bootstrap

--- Cornell Cinema/index.php
+++ Cornell Cinema/index.php
@@ -2,12 +2,21 @@
 <html>
 <head>
 	<link rel="stylesheet" href="styles/style.css">
+	<link rel="stylesheet" href="styles/bootstrap.css">
 	<title>Welcome to Cornell Cinema</title>
 </head>
 <body>
-	
-</body>
+<header>
+	<div class="container" id='logo-box'>
+		<!-- <div class="row"> -->
+    		<!-- <div class="col-lg-12"> -->
+				<img src="images/logo-spring17.png" alt="Cornell Cinema Logo" id="logo">
+			<!-- </div> -->
+		<!-- </div> -->
+	</div>
+</header>
 <footer>
 	
 </footer>
+</body>
 </html>
