9c52d32bdda3f5ae7138d3b52cb6881fc20e08bd
Annie Zhang <Annie Zhang>
2017-04-30 16:58:08 -0400

    nav bar

--- Cornell Cinema/index.php
+++ Cornell Cinema/index.php
@@ -18,8 +18,21 @@
 					<nav>
 						<ul>
 							<li><a href="index.php">Home</a></li>
-							<li><a href='films.php'>Films</a></li>
-							<li><a href='aboutus.php'>About Us</a></li>
+							<li><div class="dropdown"><button class="dropbtn">Films</button>
+								<div class="dropdown-content">
+								    <a href="#">Link 1</a>
+								    <a href="#">Link 2</a>
+								    <a href="#">Link 3</a>
+								</div>
+								</div>
+							</li>
+							<li><div class="dropdown"><button class="dropbtn">About Us</button>
+								<div class="dropdown-content">
+								    <a href="#">Link 1</a>
+								    <a href="#">Link 2</a>
+								    <a href="#">Link 3</a>
+								</div>		
+							</li>
 							<li><a href='search.php'>Search</a></li>
 						</ul>
 					</nav>
