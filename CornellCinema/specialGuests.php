5f6e7d24238eb8fbed496192ac4ea8105d69c1bf
anniehughey <agh87@cornell.edu>
2017-05-15 18:08:44 +0000

    some content

--- CornellCinema/specialGuests.php
+++ CornellCinema/specialGuests.php
@@ -21,7 +21,7 @@
 		</div>
 		<!-- end of header row div -->
 		<div class="content">
-			<h1 class="title"></h1>
+			<h1 class="title">SPECIAL GUESTS</h1>
 			<p class="text">Cornell Cinema began in 1970 as a university film society, but has evolved into a media arts center with a national reputation. Cornell Cinema's primary purpose is to expose Ithaca and regional audiences to alternative forms of cinema (such as documentary, classic, and experimental films) as well as alternative voices not usually heard through the mass media. Its secondary purpose is to provide affordable, second-run commercial entertainment for the Cornell community. In fulfillment of its educational goals, Cornell Cinema encourages audiences to reflect critically on commercial and other media by supplementing its over 300 annual screenings with occasional faculty introductions, guest filmmakers, panel discussions, and more.</p>
 
 
