3d5259a2fc62b02b1d72e6404fa1a5a84e7379cb
Annie Zhang <Annie Zhang>
2017-05-16 01:07:48 -0400

    populated ourstory and advertise pages

--- CornellCinema/advertise.php
+++ CornellCinema/advertise.php
@@ -21,11 +21,27 @@
 		</div>
 		<!-- end of header row div -->
 		<div class="row content">
-			<h1 class="title">ADVERTISE</h1>
-			<!-- Include information about onscreen advertising, including its benefits and pricing -->
-			<!-- Also include contact info for this -->
+			<h1 class="title">Why Advertise at Cornell Cinema?</h1>
 			<p class="text">
-				You should advertise with us because...
+				During the academic year, Cornell Cinema typically presents 5 different films per week, ranging from classic, to foreign, to documentary, to arthouse. These are screened in repertory style five nights a week, Wednesday through Sunday—two shows on Thursday, Friday, Saturday, and Sunday—in the historic Willard Straight Theatre on Cornell University’s campus, for a total of 9 screenings per week. The variety of films shown attracts a wide demographic of undergraduate and graduate students, Cornell faculty and staff, and community members. Average attendance per screening is 60. This kind of exposure offers advertisers a unique opportunity.
+			</p>
+			<h1 class="title">Why On-Screen Advertising?</h1>
+			<p class="text">
+				Movie-goers are a captive audience for digital on-screen advertising, and studies have shown that recall of slides is 4 times more likely than that of newspapers and 3 times more likely than television. <br>
+				Your on-screen ad will run at least twice for at least 10 seconds each in at least the 10 minutes before films screen in the Willard Straight Theatre.* This ensures your slide or short will come up while people take their seats before the movie begins.<br>
+				The cost, especially for long-term advertising, is extremely reasonable for the amount of exposure you receive.<br>
+				You are welcome to provide your own slide - or high resolution short commercial - which should measure 800 pixels wide by 600 pixels high and last no more than 15 seconds.
+			</p>
+			<h1 class="title">Our Rates</h1>
+			<p class="text">
+				$30 per week <br>
+				$200 for 4 weeks ($25 per week) <br>
+				$300 per semester <br>
+				$500 per academic year <br>
+			</p>
+			<h1 class="title">For more information, contact Douglas McLaren at dsm27@cornell.edu!</h1>
+			<p class="text">
+			**Please note that slides may not run before some special guest events. Actual time slide is projected and number of views may be more, but will not be less.
 			</p>
 			<a href="contact.php">
 				<div class="donate">
