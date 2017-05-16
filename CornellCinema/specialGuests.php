58ecaa124ab6e6f02246a0d89bc5a7f207cac924
Annie Zhang <Annie Zhang>
2017-05-16 15:26:14 -0400

    special guest page formatting

--- CornellCinema/specialGuests.php
+++ CornellCinema/specialGuests.php
@@ -24,13 +24,17 @@
 		<div class="row">
 				<p class="text">Over thirty guest filmmakers and speakers attend Cornell Cinema screenings and workshops each year, introducing and discussing their work. Previous guests have included Todd Haynes, Kelly Reichardt, Alex Rivera, Scott Ferguson '80, Will Gluck '93, Tim Squyres '81, Ernie Gehr, So Young Kim, Jennifer Reeves, Brent Green, Serge Bromberg, Howard A. Rodman '71, Sam Green, Peter Hutton, Julie Murray, Vincent Grenier, Chantal Akerman, Craig Baldwin, Irit Batsry, Stephanie Black, Robert Beavers, Sadie Benning, John Cleese, Ximena Cuevas, Su Friedrich, Miranda July, Lewis Klahr, Ang Lee, Sharon Lockhart, Anne Makepeace, Anand Patwardhan, Leighton Pierce, Daniel Reeves, Jay Rosenblatt, Thelma Schoonmaker, Roshan Seth, and Yvonne Welbon.</p>
 		</div>
-			<div class="col-sm-12 col-lg-6"> 
-			<img src="images/david-seidler.jpg" alt="David Siedler" id="siedler">
-			</div>
-			<div class="col-sm-12 col-lg-6"> 
-			<img src="images/council-of-women.jpg" alt="Council of Cornell Women" id="council-of-women">
-			</div>
-		
+		<div class="row"> 
+			<p class="text">
+				<div class="col-sm-12 col-lg-6">
+					<img src="images/david-seidler.jpg" alt="David Siedler" id="siedler" class="specialguestphoto">
+				</div>
+				<div class="col-sm-12 col-lg-6">
+					<img src="images/council-of-women.jpg" alt="Council of Cornell Women" id="council" class="specialguestphoto">
+				</div>
+			</p>
+		</div>
+		<br>
 
 		<!-- end of content div -->
 		<div class="row footer">
