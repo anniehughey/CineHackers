1f91236e92e23099694378b29bd181fcdf486664
Aaron Bu <acb256@cornell.edu>
2017-05-16 14:25:23 -0400

    specialGuest

--- CornellCinema/specialGuests.php
+++ CornellCinema/specialGuests.php
@@ -14,18 +14,34 @@
 	<script src="js/scripts.js"></script>
 	<title>Special Guests</title>
 </head>
-
 <body>
 	<div class="container">
 		<div class="row header">
 			<?php include "includes/header.php" ?>
 		</div>
 		<!-- end of header row div -->
-		<div class="content">
-			<h1 class="title">SPECIAL GUESTS</h1>
-			<p class="text">Over thirty guest filmmakers and speakers attend Cornell Cinema screenings and workshops each year, introducing and discussing their work. Previous guests have included Todd Haynes, Kelly Reichardt, Alex Rivera, Scott Ferguson '80, Will Gluck '93, Tim Squyres '81, Ernie Gehr, So Young Kim, Jennifer Reeves, Brent Green, Serge Bromberg, Howard A. Rodman '71, Sam Green, Peter Hutton, Julie Murray, Vincent Grenier, Chantal Akerman, Craig Baldwin, Irit Batsry, Stephanie Black, Robert Beavers, Sadie Benning, John Cleese, Ximena Cuevas, Su Friedrich, Miranda July, Lewis Klahr, Ang Lee, Sharon Lockhart, Anne Makepeace, Anand Patwardhan, Leighton Pierce, Daniel Reeves, Jay Rosenblatt, Thelma Schoonmaker, Roshan Seth, and Yvonne Welbon.</p>
-
-
+		<div class="row content">
+			<div class="row">
+				<h1 class="title">Special Guests</h1>
+				<div class="col-sm-12 col-lg-3">
+					<p class="text">
+						Over thirty guest filmmakers and speakers attend Cornell Cinema screenings and workshops each year, introducing and discussing their work. Previous guests have included Todd Haynes, Kelly Reichardt, Alex Rivera, Scott Ferguson '80, Will Gluck '93, Tim Squyres '81, Ernie Gehr, So Young Kim, Jennifer Reeves, Brent Green, Serge Bromberg, Howard A. Rodman '71, Sam Green, Peter Hutton, Julie Murray, Vincent Grenier, Chantal Akerman, Craig Baldwin, Irit Batsry, Stephanie Black, Robert Beavers, Sadie Benning, John Cleese, Ximena Cuevas, Su Friedrich, Miranda July, Lewis Klahr, Ang Lee, Sharon Lockhart, Anne Makepeace, Anand Patwardhan, Leighton Pierce, Daniel Reeves, Jay Rosenblatt, Thelma Schoonmaker, Roshan Seth, and Yvonne Welbon.
+					</p>
+				</div>
+				<div class="col-sm-12 col-lg-9">
+					<img src="images/david-seidler.jpg" alt="David Siedler" id="siedler">
+					<!-- img src: Cornell Cinema -->
+				</div>
+				<!--end of img div-->
+			</div>
+			<!-- end of WSH div-->
+			<div class="row">
+				<div class="col-sm-12 col-lg-9">
+					<img src="images/council-of-women.jpg" alt="Council of Cornell Women" id="council-of-women">
+					<!-- img src: Cornell Cinema -->
+				</div>
+				<!--end of img div-->
+			</div>
 		</div>
 		<!-- end of content div -->
 		<div class="row footer">
@@ -35,3 +51,4 @@
 	</div>
 	<!-- end of container div -->
 </body>
+
