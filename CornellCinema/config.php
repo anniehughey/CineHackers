9d64759a9cb86ba09a80512218f31dc18446818f
Annie Zhang <Annie Zhang>
2017-05-16 13:37:43 -0400

    sort

--- CornellCinema/config.php
+++ CornellCinema/config.php
@@ -4,5 +4,10 @@
 	define('DB_USER', 'fp_cinehckrs');
 	define('DB_PASSWORD', 'cinehackers');
 	define('DB_NAME', 'info230_SP17_fp_cinehckrs');
+	// define('DB_HOST', 'localhost');
+	// define('DB_USER', 'root');
+	// define('DB_PASSWORD', 'root');
+	// define('DB_NAME', 'info230_SP17_fp_cinehckrs');
+	// define('DB_PORT', '8889');
 
 ?>
--- CornellCinema/includes/filmsHandler.php
+++ CornellCinema/includes/filmsHandler.php
@@ -3,8 +3,51 @@
 	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); 
 	$mysqli->set_charset("utf8");
 	
-	$query = "SELECT * 
-				FROM Movies";	
+?>	<div class="row content">
+		<p class="text">
+			<form action="allFilms.php" method='post' id="sort">
+				Sort By:
+				<select name="select" id="select">
+					<option value="">Select an Option</option>
+					<option value="title">Title</option>
+					<option value="release">Release Date</option>
+					<option value="time">Runtime</option>
+				</select>
+				<select name="ascdesc" id="ascdesc">
+					<option value="asc">Ascending</option>
+					<option value="desc">Descending</option>
+				</select>
+				<input type="submit" name="sortform" value="Sort"> 
+			</form>
+			
+		</p>
+	</div>
+
+<?php	
+	$query = "SELECT * FROM Movies";	
+	if(isset($_POST['sortform'])){
+		if (!empty($_POST['select'])) {
+			$selectoption = $_POST['select'];
+			$ascdesc = $_POST['ascdesc'];
+			if ($selectoption == 'title'){
+				$query = 'SELECT * FROM movies ORDER BY title ';
+			}
+			if ($selectoption == 'release') {
+				$query = 'SELECT * FROM movies ORDER BY ReleaseYearCountry ';
+			}
+			if ($selectoption == 'time') {
+				$query = 'SELECT * FROM movies ORDER BY Runtime ';
+			}
+			if ($ascdesc == 'asc') {
+				$query = $query.'ASC';
+			}
+			if ($ascdesc == 'desc') {
+				$query = $query.'DESC';
+			}
+		} else {
+			print("<div class='row content'><p class='text'>Please select valid sort option!</p></div>");
+		}
+	}
 	$result = $mysqli->query( $query );
 	echo("<div class = 'filmContainer'>");
 	while($row = $result -> fetch_assoc()) {
