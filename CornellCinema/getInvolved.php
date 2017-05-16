9903aef43122ee9684bc763143ff31ae83804048
anniehughey <agh87@cornell.edu>
2017-05-16 02:49:51 +0000

    involvement content

--- CornellCinema/getInvolved.php
+++ CornellCinema/getInvolved.php
@@ -26,15 +26,34 @@
 			<h1 class="title">HOW TO GET INVOLVED</h1>
 			<!-- Include information about onscreen advertising, including its benefits and pricing -->
 			<!-- Also include contact info for this -->
-			<p class="text">
-				...
-			</p>
-			<a href="contact.php">
-				<div class="donate">
-					<h1 class="donateButton">WE'D LIKE TO HEAR FROM YOU</h1>
+			<div class="col-sm-12 col-lg-6">
+				<h3>Employment</h3>
+				<p class="text">Cornell Cinema hires Box and Concessions Officers, House Managers, Projectionists, Office Assistants, Shippers and more! Paid positions can be held by Cornell University students, and most require Work-Study or some other wage subsidy scholarship (Cornell Tradition or OMEA, for instance). Paid employees receive a Comp Pass for two to all regular screenings. Anyone interested in working for the Cinema is encouraged to <a href="http://cinema.cornell.edu/jobs/cinema_application.pdf">Fill Out An Application</a> whether or not there are specific positions available. Applications are kept on file for the academic year.</p>
+			</div>
+			<div class="col-sm-12 col-lg-6">
+				<h3>Volunteer Opportunities</h3>
+				<p class="text">Not a Cornell student, or you don't have Work-Study, or you just love to volunteer and want to be involved in the Cinema? Be a Volunteer Usher! Ushers work one night a week (for two films). They arrive early, help set up, rip tickets, watch the film, and help clean up at the end of the night – and, beside a free uniform (a snazzy Cinema Staff T-Shirt), the use of our industrial-strength flashlight and our unending gratitude, Ushers receive a Comp Pass for two to all regular screenings. Please fill out our <a href="http://cinema.cornell.edu/jobs/cinema_application.pdf">Employment Application</a>, noting evenings you are generally available, and drop it by the office..</p>
+			</div>
+			<a href="http://cinema.cornell.edu/jobs/cinema_application.pdf">
+				<div class="row donate">
+					<h1 class="donateButton">APPLY NOW!</h1>
 				</div>
 			</a>
-			<!-- end of donate div-->
+			<!-- end of apply div-->
+			<div class="col-sm-12 col-lg-6">
+				<h3>Student Advisory Board</h3>
+				<p class="text">The Cornell Cinema Student Advisory Board is an organization of students, both undergraduate and graduate, whose purpose is to represent and promote Cornell Cinema, as well as to advise and assist Cornell Cinema in its overall mission to offer the Cornell and greater Ithaca community a wide variety of cinematic and media experiences, for both educational and entertainment purposes. Specific committees work on promotion, event planning and execution, and programming. The first meeting of the SAB (Fall 2016) takes place on Wednesday, September 13 at 5pm in Willard Straight Theatre. All students are welcome to attend and learn more! (The board typically meets on the first Tuesday of the month at 5pm, thereafter.).</p>
+			</div>
+			<div class="col-sm-12 col-lg-6">
+				<h3>Cosponsorship Program</h3>
+				<p class="text">Cornell Cinema accepts a limited number of proposals from faculty members, campus departments and programs, student organizations and community groups for incorporation into Cornell Cinema's schedule. Proposals most likely to be accepted are those with broad audience appeal, where work is available in a higher quality format than DVD, and when being requested for a class, enrollment is at least 40 and students are required to attend the screenings. Proposals that come with the offer of financial assistance stand a better chance of being accepted. Proposals typically need to be submitted a semester in advance. Inquiries can be sent to Cinema Director Mary Fessenden at mkf2@cornell.edu. If your proposal is not accepted for inclusion in Cornell Cinema's schedule, you may still be able to have the work shown in Willard Straight Theatre at a time when Cornell Cinema is not presenting its own programs (primarily Monday and Tuesday nights) for a reasonable rental fee. These events can be scheduled with less lead time, but typically no less than three weeks. For details on these special rentals, contact Cinema Manager Douglas McLaren at dsm27@cornell.edu.</p>
+			</div>
+			<a href="http://cinema.cornell.edu/jobs/cinema_application.pdf">
+				<div class="row donate">
+					<h1 class="donateButton">SUGGEST A MOVIE FOR NEXT SEMESTER</h1>
+				</div>
+			</a>
+			<!-- end of SUGGESTION div-->
 
 		</div>
 		<!-- end of content div -->
